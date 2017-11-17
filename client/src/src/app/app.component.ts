0789723f5c1069519b67ac5fa1f77a6476ae72ba
Abdullah Bawazir <shawazir@gmail.com>
2017-11-17 16:50:16 +0300

    Undo later

--- client/src/src/app/app.component.ts
+++ client/src/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+import {environment} from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  constructor(private httpClient: HttpClient) { }
+
+  serverResponse: string;
+
+  ngOnInit(): void {
+    // Make the HTTP request:
+    this.httpClient.get(environment.API_ROOT_URL + '/cache/faqs').subscribe(data => {
+      // Read the result field from the JSON response.
+      this.serverResponse = data + '';
+    });
+  }
+
+  // title = 'app';
 }
--- server/src/main/java/com/elm/ssp/registration/web/controller/CacheController.java
+++ server/src/main/java/com/elm/ssp/registration/web/controller/CacheController.java
@@ -1,5 +1,7 @@
 package com.elm.ssp.registration.web.controller;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +17,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class CacheController {
 
 	@RequestMapping(value = "/cache/faqs", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<String> getFaqs() {
+	public ResponseEntity<String> getFaqs(HttpServletResponse response) {
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+		response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 		return new ResponseEntity<String>("Spring works :)", HttpStatus.OK);
 	}
 }
